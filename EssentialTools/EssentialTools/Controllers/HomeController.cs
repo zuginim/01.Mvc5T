557b3b83df25a0657c930ef0f599ed34cbae0d2b
jiwook_NoteBook <zugi@outlook.kr>
2016-08-24 17:16:33 +0900

    16.08.24

--- EssentialTools/EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EssentialTools.Models;
+//16.08.24 Ninject 추가.
+using Ninject;
 
 namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        private IValueCalculator calc;
+
         private Product[] products = {
             new Product { Name="Kayak", Category="Watersport", Price = 275M},
             new Product { Name="Lifejacket", Category="Watersport", Price = 48.95M},
@@ -16,9 +20,17 @@ namespace EssentialTools.Controllers
             new Product { Name="Corner flag", Category="Soccer", Price = 34.95M}
         };
 
-        public ActionResult Index() {
+        public HomeController(IValueCalculator calcParam) {
+            calc = calcParam;
+        }
 
-            IValueCalculator calc = new LinqValueCalculator();
+        public ActionResult Index() {
+            
+            /*16.08.24 NuGet을 이용한 ninject 설치후 사용. (LinqValueCalculator.cs 모델과, Homecontroller 간의 의존성 제거.)
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+            */
 
             ShoppingCart cart = new ShoppingCart(calc) { Products = products };
 
--- EssentialTools/EssentialTools/Models/LinqValueCalculator.cs
+++ EssentialTools/EssentialTools/Models/LinqValueCalculator.cs
@@ -15,6 +15,10 @@ namespace EssentialTools.Models
 
     //16.08.24 interface 도입.
     public class LinqValueCalculator : IValueCalculator {
+
+        private IDiscountHelper discounter;
+        public LinqValueCalculator(IDiscountHelper discountParam) { }
+
         public decimal ValueProducts(IEnumerable<Product> products) {
             return products.Sum(p=>p.Price);
         }
