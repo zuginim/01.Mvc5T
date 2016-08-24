de4f3bfb997cfbbae3cd07b64624c9f926576f8a
jiwook_NoteBook <zugi@outlook.kr>
2016-08-24 15:13:36 +0900

    16.08.24

---
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products = {
+            new Product { Name="Kayak", Category="Watersport", Price = 275M},
+            new Product { Name="Lifejacket", Category="Watersport", Price = 48.95M},
+            new Product { Name="Soccer ball", Category="Soccer", Price = 19.50M},
+            new Product { Name="Corner flag", Category="Soccer", Price = 34.95M}
+        };
+
+        public ActionResult Index() {
+
+            LinqValueCalculator calc = new LinqValueCalculator();
+
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+
+            decimal totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
+        }
+        
+        public ActionResult Exam() {
+
+            //16.08.24 의문1 : 이렇게 해도 되는거 같은데 왜 구지 위와같이 하는건지..
+            LinqValueCalculator calc_exam = new LinqValueCalculator();
+            decimal test = calc_exam.ValueProducts(products);
+            return View(test);
+        }
+
+        public ActionResult Exam2() {
+
+            //16.08.24 의문2 : 이렇게 해도 되는거 같은데 왜 구지 위와같이 하는건지..
+            ShoppingCart_Exam cart_exam = new ShoppingCart_Exam() { Products = products };
+            decimal test2 = cart_exam.CalculateProductTotal();
+            return View(test2);
+        }
+    }
+}
---
+++ EssentialTools/EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        //16.08.24 price 합계계산클래스. - 기존.
+        private LinqValueCalculator calc;
+
+        //16.08.24 ShoppingCart 생성자에 형 세팅.
+        public ShoppingCart(LinqValueCalculator calcParam) {
+            calc = calcParam;
+        }
+        
+        //16.08.24 IEnumerable 형식의 속성.get;set;
+        public IEnumerable<Product> Products { get; set; }
+
+        //16.08.24 합계를 구하는 메서드 호출.
+        public decimal CalculateProductTotal() {
+            return calc.ValueProducts(Products);
+        }
+    }
+    public class ShoppingCart_Exam {
+        LinqValueCalculator calc = new LinqValueCalculator();
+        public IEnumerable<Product> Products { get; set; }
+        public decimal CalculateProductTotal() {
+            return calc.ValueProducts(Products);
+        }
+    }
+}
