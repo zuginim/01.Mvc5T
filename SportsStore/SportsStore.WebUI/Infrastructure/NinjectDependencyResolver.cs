a2106d534c4b3c7ec0363dd297fd842a91541022
jiwook_NoteBook <zugi@outlook.kr>
2016-08-24 17:52:05 +0900

    16.08.24

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam) {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType) {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType) {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings() {
+
+            //16.08.24 이곳에 바인딩정보 구현.
+        }
+    }
+}
