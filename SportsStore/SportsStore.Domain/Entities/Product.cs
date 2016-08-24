866950f0a2f90188c29c992ce26e9d008197721c
jiwook_NoteBook <zugi@outlook.kr>
2016-08-24 18:05:25 +0900

    16.08.24

---
+++ SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Product
+    {
+        //16.08.24 상품속성.
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
