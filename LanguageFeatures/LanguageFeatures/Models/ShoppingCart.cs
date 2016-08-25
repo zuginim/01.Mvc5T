ea2f708e60c03181674d865d79d01a47ba58225a
jiwook_NoteBook <zugi@outlook.kr>
2016-08-25 17:33:57 +0900

    16.08.25

--- LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
+++ LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
@@ -9,8 +9,5 @@ namespace LanguageFeatures.Models
     {
         //16.08.21 Product 개체의 컬렉션.
         public List<Product> Products { get; set; }
-
-        //16.08.21 인터페이스 확장메서드, 필터링 확장메서드, 델리게이트,람다식 확장메서드, 자동형식추론 알아봐야함.
-        //16.08.21 비동기메서드에 대해 좀더 자세히 알아봐야함.
     }
 }
